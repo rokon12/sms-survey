e54fccba855524ee0aa03be09555c1d8c3008670
Bazlur Rahman <anmbrr.bit0112@gmail.com>
2018-07-19 15:20:03 +0600

    Add home endPoint

--- src/main/java/com/bkash/smsdatacollection/SmsDataCollectionApplication.java
+++ src/main/java/com/bkash/smsdatacollection/SmsDataCollectionApplication.java
@@ -2,11 +2,24 @@ package com.bkash.smsdatacollection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class SmsDataCollectionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmsDataCollectionApplication.class, args);
+    }
+
+    @Controller
+    public class HomeController{
+        @GetMapping("/")
+        @ResponseBody
+        public  String index(){
+
+            return "It works!";
+        }
     }
 }
